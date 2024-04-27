da4be011506c80d05e1f987b49ba6a1c5be76cb4
Klemen Tušar <techouse@gmail.com>
2024-04-27 12:09:32 +0100

    :white_check_mark: fix comparison tests

--- tests/comparison/qs.py
+++ tests/comparison/qs.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python3
 
-import json
-import os
-import sys
-from pathlib import Path
-
-
-sys.path.append(str(Path(f"{__file__}").parent.parent))
-
-import qs_codec
+from json import loads, dumps
+from os.path import dirname, join, realpath
+from qs_codec import encode, decode
 
 
 def main():
     # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.realpath(__file__))
+    script_dir = dirname(realpath(__file__))
 
     # Construct the path to the JSON file
-    file_path = os.path.join(script_dir, "test_cases.json")
+    file_path = join(script_dir, "test_cases.json")
 
     # Read the JSON file
     with open(file_path, "r") as file:
         contents = file.read()
 
     # Parse the JSON contents into a list of dictionaries
-    e2e_test_cases = json.loads(contents)
+    e2e_test_cases = loads(contents)
 
     # Iterate over the test cases
     for test_case in e2e_test_cases:
         # Encode the 'data' field
-        encoded = qs_codec.encode(test_case["data"])
+        encoded = encode(test_case["data"])
         # Decode the 'encoded' field
-        decoded = qs_codec.decode(test_case["encoded"])
+        decoded = decode(test_case["encoded"])
         # Print the results
         print("Encoded:", encoded)
-        print("Decoded:", json.dumps(decoded, separators=(",", ":")))
+        print("Decoded:", dumps(decoded, separators=(",", ":")))
 
 
 if __name__ == "__main__":
